c3038fcf588d270c1eb40b8299d470ab2046772a
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-08-28 20:44:16 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { fetchData } from "api/api";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Loader } from "./Loader/Loader";
@@ -8,23 +8,22 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-export class App extends Component {
-  state = {
-    searcheQuery: '',
-    page: 1,
-    total: 0,
-    dataImages: [],
-    isLoading: false,
-    error: null,
-  }
+export const App = () => {
+  const [searcheQuery, setSearcheQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [dataImages, setDataImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async componentDidUpdate(_, prevState) {
-    const { searcheQuery, page } = this.state;
-    if (prevState.searcheQuery !== searcheQuery || prevState.page !== page) {
-      this.setState({ isLoading: true });
+  
+  useEffect(() => {
+    if (!searcheQuery)
+      return;
+    const imagesApi = async () => {
       try {
+        setIsLoading(true);
         const data = await fetchData(searcheQuery, page);
-
         if (data.totalHits === 0) {
           toast.warning('Not a valid request. Please enter a valid value!', {
             autoClose: 1000,
@@ -40,52 +39,56 @@ export class App extends Component {
             theme: 'colored',
           });
         }
-        this.setState(prevState => ({
-          dataImages: [...prevState.dataImages, ...data.hits],
-          total: data.totalHits,
-        }));
+        setDataImages(prevState => [...prevState, ...data.hits]);
+        setTotal(data.totalHits);
       } catch (error) {
-        this.setState({ error });
+        setError(error.message);
         toast.error(error.message);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
+    };
+    imagesApi();
+  }, [searcheQuery, page]);
+
+  const handleFormSubmit = searcheQuerySubmit => {
+    if (searcheQuery !== searcheQuerySubmit) {
+      setSearcheQuery(searcheQuerySubmit);
+      setPage(1);
+      setDataImages([]);
     }
-  }
-
-  handleFormSubmit = searcheQuery => {
-    this.setState({ searcheQuery, page: 1, dataImages: [] })
-  }
-
-  buttonLoadMore = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
   };
 
-  render() {
-    const { dataImages, isLoading, total } = this.state;
-    return (
-      <>
-        <Searchbar handleFormSubmit={ this.handleFormSubmit } />
-        {isLoading && <Loader />}
-        {dataImages.length > 0 && <ImageGallery images={dataImages} />}
-        {!isLoading && total !== dataImages.length && (
-          <Button onClick={this.buttonLoadMore} />
-        )}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </>
-    )
-  }
-}
+  const buttonLoadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  return (
+    <>
+      <Searchbar handleFormSubmit={handleFormSubmit} />
+      {!error && (
+        <div>
+          <p>{error}</p>
+        </div>
+      )}
+      {isLoading && <Loader />}
+      {dataImages.length > 0 && <ImageGallery images={dataImages} />}
+      {!isLoading && total !== dataImages.length && (
+        <Button onClick={buttonLoadMore} />
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+};
+
